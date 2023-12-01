37e00f3018f903ca660208d78be2fc41fa8d72a5
claudiugeorgeavram <claudiu_george.avram@stud.acs.upb.ro>
2023-12-01 14:30:28 +0200

    solved all seg faults

--- content/assignments/parallel-graph/src/os_threadpool.c
+++ content/assignments/parallel-graph/src/os_threadpool.c
@@ -9,18 +9,12 @@
 os_task_t *task_create(void *arg, void (*f)(void *))
 {
 	/* TODO: Implement task creation. */
-	printf("task_create declare\n");
 	os_task_t *task;
-	printf("task_create after declare\n");
 
-	printf("task_create calloc\n");
 	task = calloc(1, sizeof(*task));
-	printf("task_create after calloc\n");
 
-	printf("task_create assign\n");
 	task->argument = arg;
 	task->task = f;
-	printf("task_create after assign\n");
 
 	return task;
 }
@@ -29,19 +23,33 @@ os_task_t *task_create(void *arg, void (*f)(void *))
 void add_task_in_queue(os_threadpool_t *tp, os_task_t *t)
 {
 	/* TODO: Implement adding new task in queue. */
-	printf("before add task lock\n");
+	printf("before declare\n");
+	os_task_queue_t *new_node, *node_iter;
+	printf("before lcok\n");
 	pthread_mutex_lock(&(tp->lock));
-	printf("after add task lock\n");
 
-	printf("before create node\n");
-	os_task_queue_t *new_node = calloc(1, sizeof(*new_node));
-	new_node->task = t;
-	tp->tasks->next = new_node;
+	printf("before if\n");
+	if(tp->tasks == NULL) {
+		printf("first task added\n");
+		tp->tasks->task = t;
+		tp->tasks->next = NULL;
+	} else {
+		node_iter = tp->tasks;
+		while (node_iter->next != NULL) {
+			node_iter = node_iter->next;
+		}
+		printf("another task added\n");
+		new_node = calloc(1, sizeof(*new_node));
+		printf("calloc done\n");
+		new_node->task = t;
+		new_node->next = NULL;
+		printf("added task in new node\n");
+		node_iter->next = new_node;
+		printf("added new node as next\n");
+	}
 
-	printf("after create node\n");
 
 	pthread_mutex_unlock(&(tp->lock));
-	printf("after add task unlock\n");
 }
 
 /* Get the head of task queue from threadpool */
@@ -51,26 +59,19 @@ os_task_t *get_task(os_threadpool_t *tp)
 	os_task_t *t;
 	os_task_queue_t *old_node;
 
-	
-	printf("get_task before lock\n");
-	pthread_mutex_lock(&(tp->lock));
-	printf("get_task after lock\n");
 
-	printf("get_task before t assign\n");
+	pthread_mutex_lock(&(tp->lock));
+
+	if (tp->tasks->task == NULL) {
+		pthread_mutex_unlock(&(tp->lock));
+		return NULL;
+	}
 	t = tp->tasks->task;
-	printf("get_task after t assign\n");
-	printf("get_task before oldnode assign\n");
 	old_node = tp->tasks;
-	printf("get_task after oldnode assign\n");
-	printf("get_task before tp tasks free assign\n");
 	tp->tasks = tp->tasks->next;
-	printf("get_task after tp tasks free assign\n");
-	printf("get_task before free mem\n");
 	free(old_node);
-	printf("get_task after free mem\n");
 
 	pthread_mutex_unlock(&(tp->lock));
-	printf("get_task after mutex lock\n");
 
 	return t;
 }
@@ -78,10 +79,11 @@ os_task_t *get_task(os_threadpool_t *tp)
 /* Initialize the new threadpool */
 os_threadpool_t *threadpool_create(unsigned int num_tasks, unsigned int num_threads)
 {
-	printf("init\n");
 	/* TODO: Implement thread pool creation. */
 	os_threadpool_t *tp;
 	tp = calloc(1, sizeof(*tp));
+
+	pthread_mutex_init (&(tp->lock), NULL);
 	
 	/* Initialize tasks */
 	tp->tasks = calloc(num_tasks, sizeof(tp->tasks));
@@ -101,13 +103,10 @@ os_threadpool_t *threadpool_create(unsigned int num_tasks, unsigned int num_thre
 void *thread_loop_function(void *args)
 {
 	/* TODO: Implement thread loop function. */
-	printf("thread_loop_start\n");
 	os_threadpool_t *tp = (os_threadpool_t *)args;
-	printf("thread_loop init args\n");
 	os_task_t *t;
 
 	while (!tp->should_stop) {
-		printf("thread_loop in while\n");
 		t = get_task(tp);
 		if (t != NULL) {
 			t->argument;
